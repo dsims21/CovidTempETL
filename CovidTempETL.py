15df1de911dbaea010588576ff2a9b2005a6442f
Daniel Sims <dsims@spearhead-capm.com>
2020-05-14 17:50:11 -0700

    For loop to convert date

--- CovidTempETL.py
+++ CovidTempETL.py
@@ -1,4 +1,9 @@
 import pandas as pd
+from datetime import datetime
+
+def convert_to_date(inputDate):
+    date = datetime.strptime(str(inputDate), '%Y%m%d')
+    return datetime.strftime(date,'%Y-%m-%d')
 
 
 colnames=['station_id', 'date', 'observation_type', 'observation_value']
@@ -10,5 +15,11 @@ covidDF = pd.read_csv("us-counties.csv",)
 
 
 
+#Changing date format to match other files.
+#There's probably a better way to do this.
+#weatherDF['date'] = weatherDF['date'].apply(convert_to_date)
 for index, row in weatherDF.iterrows():
-    weatherDF.loc[weatherDF.index[index], 'Country'] = 1
+    date = datetime.strptime(str(weatherDF.loc[weatherDF.index[index], 'date']), '%Y%m%d')
+    weatherDF.loc[weatherDF.index[index], 'date'] = datetime.strftime(date,'%Y-%m-%d')
+
+print('hi')
