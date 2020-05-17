0b2bc4856e65d771a0b0673b93aa28d794670d85
Daniel Sims <dsims@spearhead-capm.com>
2020-05-17 15:37:49 -0700

    Utilizing function for date conversion

--- CovidTempETL.py
+++ CovidTempETL.py
@@ -19,7 +19,6 @@ stationsDF = pd.read_csv("ground_station_custom_locations_world.csv",)
 url = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv'
 covidDF = pd.read_csv(url)
 
-
 ### TRANSFORM ###
 
 # Adding the word 'county' to match other files.
@@ -27,9 +26,7 @@ covidDF['county'] = covidDF['county'].astype(str) + ' county'
 
 # Changing date format to match other files.
 for index, row in weatherDF.iterrows():
-    date = datetime.strptime(str(weatherDF.loc[weatherDF.index[index], 'date']), '%Y%m%d')
-    weatherDF.loc[weatherDF.index[index], 'date'] = datetime.strftime(date,'%Y-%m-%d')
-
+    weatherDF.loc[weatherDF.index[index], 'date'] = convert_to_date(str(weatherDF.loc[weatherDF.index[index], 'date']))
 
 ### Filter ###
 
@@ -43,5 +40,8 @@ stationsDF.dropna(inplace=True)
 
 
 ### JOIN ###
+
+#Start by left joining
+joinedDF = weatherDF.merge(stationsDF, left_on='station_id', right_on='ID')
 
 print('Done')
