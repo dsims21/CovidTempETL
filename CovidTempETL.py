36dafd69fb07a32a3580acc6b7db27e69192abf6
Daniel Sims <dsims@spearhead-capm.com>
2020-05-17 16:40:27 -0700

    Filtering Weather (messy still)

--- CovidTempETL.py
+++ CovidTempETL.py
@@ -1,14 +1,14 @@
 import pandas as pd
 from datetime import datetime
 
-counter  =0
+counter = 0
 
 def convert_to_date(inputDate):
-    date = datetime.strptime(str(inputDate), '%Y%m%d')
     global counter
     counter += 1
     print(counter)
-    return datetime.strftime(date,'%Y-%m-%d')
+    return datetime.strftime(datetime.strptime(str(inputDate),
+                                               '%Y%m%d'),'%Y-%m-%d')
 
 
 ### EXTRACT ##
@@ -24,6 +24,23 @@ stationsDF = pd.read_csv("ground_station_custom_locations_world.csv",)
 url = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv'
 covidDF = pd.read_csv(url)
 
+### Filter ###
+
+# Filter Stations to US only
+filter = stationsDF["Country"]=="United States of America"
+stationsDF.where(filter, inplace = True)
+stationsDF.dropna(inplace=True)
+
+# Filter to US weather only
+weatherDF = weatherDF.merge(stationsDF, left_on='station_id', right_on='ID')
+filter = weatherDF['Country']=='United States of America'
+weatherDF.where(filter, inplace = True)
+weatherDF.dropna(inplace=True)
+
+#THE ABOVE FILTERS ARE A LITTLE MESSY - THERE IS STILL A FILTER SECTION BELOW TOO.
+
+
+
 ### TRANSFORM ###
 
 # Adding the word 'county' to match other files.
@@ -32,15 +49,6 @@ covidDF['county'] = covidDF['county'].astype(str) + ' county'
 # Changing date format to match other files.
 # (This currently takes a long time.)
 weatherDF['date'] = weatherDF['date'].apply(convert_to_date)
-
-
-### Filter ###
-
-# Filter Stations to US only
-filter = stationsDF["Country"]=="United States of America"
-stationsDF.where(filter, inplace = True)
-stationsDF.dropna(inplace=True)
-
 
 ### AGGREGATE ### (Do we need to so some averaging of temps?)
 
